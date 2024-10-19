a528dbb68e526d3fb58671d22ceb16a9677e5932
Richard Chovanec <RichardChovanec123@serznam.cz>
2024-10-19 15:57:36 +0200

    refactor 4

--- UPR/ukol4.c
+++ UPR/ukol4.c
@@ -1,91 +1,39 @@
 #include <stdio.h>
 
-void invalidCounter(int count);
+void printMatches(int count);
 int digitCounter(int number);
-void VerticalCounter(int* count);
+void printHorizontalMatches(int range, int* match);
+void printVerticalMatches(int* match);
 
-int main() {
-
+int main()
+{
     char mod = '0';
     int numbers = 0;
     int range = 0;
-    int invalid = 0;
-    int digitFlag = 0;
-    int max = 0;
-    
+
     scanf("%c", &mod);
     scanf("%d %d", &numbers, &range);
 
-    int histogram[9];
-    int match[9] = {0};
-    for (int i = 0; i < 9; i++) {
-        histogram[i] = i + range;
-        if(digitFlag < digitCounter(histogram[i])) {
-            digitFlag = digitCounter(histogram[i]);
-        }
-    }
+    int match[10] = {0};
 
     for (int i = 0; i < numbers; i++) {
         int tmp = 0;
         scanf("%d", &tmp);
         if (tmp < range || tmp > range + 8) {
-            invalid++;
+            match[9] += 1;
         }
-        for (int i = 0; i < 9; i++) {
-            if (histogram[i] == tmp) {
-                match[i] += 1;
-            }
+        else {
+            match[tmp-range] += 1;
         }
     }
 
     switch (mod) {
         case 'h':
-
-        for (int i = 0; i < 9; i++) {
-            if (digitFlag > digitCounter(histogram[i])) {
-                printf(" ");
-            }
-            printf("%d", histogram[i]);
-            if (match[i] > 0) {
-                printf(" ");
-                invalidCounter(match[i]);
-            }
-            else {
-                printf("\n");
-            }
-        }
-        if (invalid > 0) {
-            printf("invalid: ");
-            invalidCounter(invalid);
-        }
+        printHorizontalMatches(range, match);
         break;
 
         case 'v':
-        max = invalid;
-        for (int i = 0; i < 9; i++){
-            if (max < match[i]) {
-                max = match[i];
-            }
-        }
-        
-        invalid = max - invalid;
-        for (int i = 0; i < 9; i++){
-            match[i] = max - match[i];
-        }
-
-        for (int i = 0; i < max; i++) {
-            VerticalCounter(&invalid);
-            VerticalCounter(&match[0]);
-            VerticalCounter(&match[1]);
-            VerticalCounter(&match[2]);
-            VerticalCounter(&match[3]);
-            VerticalCounter(&match[4]);
-            VerticalCounter(&match[5]);
-            VerticalCounter(&match[6]);
-            VerticalCounter(&match[7]);
-            VerticalCounter(&match[8]);
-            printf("\n");
-        }
+        printVerticalMatches(match);
         printf("i123456789\n");
         break;
 
@@ -97,7 +45,7 @@ int main() {
     return 0;
 }
 
-void invalidCounter(int count) {
+void printMatches(int count) {
     for (int i = 0; i < count; i++) {
         printf("#");
     }
@@ -116,15 +64,47 @@ int digitCounter(int number) {
     return count;
 }
 
-void VerticalCounter(int* count) {
-    if (*count <= 0) {
-        printf("#");
+void printHorizontalMatches(int range, int* match) {
+    int maxLen = digitCounter(range + 8);
+    for (int i = 0; i < 9; i++) {
+        printf("%*d", maxLen, range + i);
+        if (match[i] > 0) {
+            printf(" ");
+            printMatches(match[i]);
+        }
+        else {
+            printf("\n");
+        }
     }
-    else {
-        printf(" ");
+    if (match[9] > 0) {
+        printf("invalid: ");
+        printMatches(match[9]);
     }
-    *count -= 1;
 }
 
+void printVerticalMatches(int* match) {
+    int maxHeight = match[9];
+    for (int i = 0; i < 9; i++){
+        if (maxHeight < match[i]) {
+            maxHeight = match[i];
+        }
+    }
 
-
+    for (int row = 0; row < maxHeight; row++) {
+        if ((maxHeight - row) <= match[9]) {
+            printf("#");
+        }
+        else {
+            printf(" ");
+        }
+        for (int i = 0; i <= 8; i++) {
+            if ((maxHeight - row) <= match[i]) {
+                printf("#");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
