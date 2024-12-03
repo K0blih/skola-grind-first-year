6bffa94422c52133df3f96157e4fb30b4bd6ae08
K0blih <RichardChovanec123@seznam.cz>
2024-12-03 20:01:31 +0100

    cooked test

--- FPR/ukolTest.hs
+++ FPR/ukolTest.hs
@@ -45,31 +45,37 @@ arrow = [ "....#....",
 
 printMaze :: Maze -> IO ()
 printMaze x = putStr (concat (map (++"\n") x))
+-- printMaze sample1 
 
 above :: Maze -> Maze -> Maze
 above x y = x ++ y
+-- printMaze(above arrow arrow)
 
 sideBySide :: Maze -> Maze -> Maze
 sideBySide x y = zipWith (++) x y
-
+-- printMaze(sideBySide arrow arrow)
 
 rotateR :: Maze -> Maze
 rotateR [x] = [x]
 rotateR xs
     | any null xs = [] 
     | otherwise = reverse (map head xs) : rotateR (map tail xs)
+-- printMaze (rotateR arrow)  
 
 rotateL :: Maze -> Maze
 rotateL [x] = [x]
 rotateL xs 
     |any null xs = []
     |otherwise=rotateL (map tail xs) ++ [map head xs]
+-- printMaze(rotateL arrow)
 
 getFromMaze :: Maze -> (Int, Int) -> Char
 getFromMaze maze (r, c) = (maze !! r) !! c
+-- getFromMaze  sample1  (1,1)
 
 putIntoMaze:: Maze -> [(Int,Int,Char)] -> Maze
 putIntoMaze maze updates = foldl splitMaze maze updates
+-- printMaze(putIntoMaze sample2 [(0,0,'1'),(6,6,'2'),(0,6,'3')]) 
 
 splitMaze :: Maze -> (Int, Int, Char) -> Maze
 splitMaze maze (r,c,char) = take r maze ++ [placeChar (maze !! r) (r,c,char)] ++ drop (r+1) maze
@@ -79,6 +85,7 @@ placeChar maze (r,c,char) = take c maze ++ [char] ++ drop (c+1) maze
 
 getPart :: Maze -> (Int, Int) -> (Int, Int) -> Maze
 getPart maze (startRow, startCol) (height, width) = [take width (drop startCol row) | row <- take height (drop startRow maze)]
+-- printMaze(getPart sample1 (1,1) (7,7))
 
 solveMaze :: Maze -> Int
 solveMaze maze = bfs [(startRow, startCol, 0)]
@@ -93,3 +100,4 @@ solveMaze maze = bfs [(startRow, startCol, 0)]
       | otherwise = bfs (queue ++ [(nr, nc, steps + 1) | (nr, nc) <- [(r-1, c), (r+1, c), (r, c-1), (r, c+1)],
                                                           nr >= 0, nr < length maze, nc >= 0, nc < length (head maze),
                                                           maze !! nr !! nc /= '*', notElem (nr, nc) (map (\(x, y, _) -> (x, y)) queue)])
+-- solveMaze sample4 
